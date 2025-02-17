1fcdd16621515fd5a0ac23c3aa393156c90de0ec
haoyu <jonhuo11@gmail.com>
2025-02-17 02:38:25 -0500

    union find simple

--- run_test.sh
+++ run_test.sh
@@ -2,14 +2,7 @@
 
 set -e  # Exit on error
 
-# Ensure a test file argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <test_file.cc>"
-    exit 1
-fi
-
-test_file="$1"
-executable_name="${test_file%.cc}.out"
+executable_name="testtmp.out"
 
 # Compile the specified test file along with all .cc files
 g++ -std=c++23 -Wall -Wextra -Werror -o "$executable_name" *.cc \
